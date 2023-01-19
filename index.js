4f7f1ce2c351c1e648a5f043683e3e3ebe57e47e
Samuel Ricardo P. Galvão <104034355+samuelgalvao@users.noreply.github.com>
2023-01-19 18:12:43 -0300

    some changes

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const c = console.log
 
-const url = 'http://api.weatherapi.com/v1/current.json?key=878f515b0dee4c9b8b3185638231701&q=Brazil, Bahia, Campo alegre de Lourdes&aqi=no'
-
-async function getData(){
+async function getData(url){
     const response = await fetch(url);
     const data = await response.json()
     return data
 }
-getData().then((data) => c(data))
+
+function getLocation(){
+    var loc = document.getElementById('location-id')
+
+    const url = `http://api.weatherapi.com/v1/current.json?key=878f515b0dee4c9b8b3185638231701&q=Brazil, Bahia, ${loc.value}&aqi=no`
+
+    // Campo Alegre de Lourdes
+
+    getData(url).then((value) => {
+        c('work');
+        c(value.current)
+    })
+}
